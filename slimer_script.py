824e52ebd35d34ea288641f48e69fc8a474c2c9b
IamTerror <32469633+IamTerror@users.noreply.github.com>
2018-03-06 19:45:28 +0100

    English translation

--- slimer_script.py
+++ slimer_script.py
@@ -5,7 +5,7 @@
 # /____  >____/__|__|_|  /\___  >__|    /____  >\___  >__|  |__|   __/|__|
 #      \/              \/     \/             \/     \/         |__|
 
-# Auteur :
+# Author :
 # +-+-+-+-+-+-+-+-+-+
 # |I|A|m|T|e|r|r|o|r|
 # +-+-+-+-+-+-+-+-+-+
@@ -21,7 +21,8 @@
 #  0. You just DO WHAT THE FUCK YOU WANT TO.
 
 # Notes :
-# Script testé sous Windows 10. Fonctionnement possible sous OS Unix sous réserve d'adaptations mineures du script.
+# Python script tested on Windows 10. It can run on Unix OS with minor adjustements.
+
 
 ########################################################################################################################
 
@@ -34,31 +35,31 @@ import os
 import time
 import shutil
 
-# VARIABLES GLOBALES ---------------------------------------------------------------------------------------------------
+# GLOBAL VARIABLES -----------------------------------------------------------------------------------------------------
 
-# paths designe un dictionnaire de pathx représentant...
-# ... le ou les répertoire(s) pathx dont on veut sauvegarder les dossiers et les fichiers
-# Ajoutez autant de paths que de répertoires dont on veut sauvegarder le contenu
+# paths denotes a dictionnary of pathx representing...
+# the directory(ies) pathx which you want to backup folders and files
+# Add as many paths as directories which you want to backup the content
 paths = {'path1': 'f:/'}
 
-# file designe le fichier dans lequel on sauvegardera les données
-# Le nom de ce fichier sera crée grâce à la fonction formatFileName()
+# file denotes the file in which you backup datas
+# This file name will build by formatFileName() function
 file = ''
 
-# currentDirectory designe le repertoire dans lequel on placera le fichier file
+# currentDirectory denotes the directory in which you will put the file file
 currentDirectory = 'f:/Log'
 
-# otherDirectories designe un dictionnaire de repertoires odx représentant le ou les répertoire(s) odx ...
-# ... dans lesquels on veut placer notre fichier de sauvegarde file, en plus de currentDirectory
-# Il est ainsi possible par exemple d'effectuer la sauvegarde du fichier file sur deux disques durs différents
-# Si vous ne souhaitez pas utiliser cette fonctionnalité, il est nécessaire de...
-# ... laisser un couple clé + valeur vide dans la variable otherDirectories : otherDirectories = {'': ''}
+# otherDirectories denotes a dictionnary of directories odx representing the directory(ies) odx...
+# in which you want to put the backup file file, in addition to currentDirectory
+# In this way, it's possible for example to put the backup file file in two differents hard drives
+# In you don't want to use this feature, you must leave a pair of...
+# ... one key + one value in the dictionnary otherDirectories, like this : otherDirectories = {'': ''}
 otherDirectories = {'od1': 'c:/Log'}
 
 
-# FONCTIONS ------------------------------------------------------------------------------------------------------------
+# FUNCTIONS ------------------------------------------------------------------------------------------------------------
 
-# Horodatage du nom de fichier de sauvegarde
+# Timestamping of the backup file name
 def formatFileName():
     currentDate = time.strftime("%Y%m%d")
     currentTime = time.strftime("%H%M%S")
@@ -66,69 +67,69 @@ def formatFileName():
     return formatFileName
 
 
-# Parsing et affichage des répertoires, sous-répetoires et descendants du dossier sélectionné (path)
+# Parsing and displaying of folders, subfolders and descendants of the selectionned folder (path)
 def parseDirs(path):
     nbDirectories = 0
     for root, dirs, files in os.walk(path):
         file.write("\n" + root)
         nbDirectories += 1
-    file.write("\n\nNombre de dossiers dans le répertoire " + path + " : " + str(nbDirectories - 1))
+    file.write("\n\nnumber of folders in the directory " + path + " : " + str(nbDirectories - 1))
 
 
-# Même chose que pour la fonction parseDirs() + parsing et affichage des noms des fichiers
+# Same as for the function parseDirs() + parsing and displaying of names' files
 def parseAllFoldersAndFiles(path):
     nbFiles = 0
     for root, dirs, files in os.walk(path):
         file.write("\n" + root)
         for filename in files:
-            # La méthode getsize() doit prendre en paramètre le chemin complet d'un fichier, c'est à dire sa racine...
-            # ... + son nom, d'où l'utilisation de la méthode join pour concaténer les deux paramètres
-            # getsize() renvoie un résultat en octets, on divise par 1024 pour convertir en ko...
-            # ... puis on ajoute 1 ko pour arrondir au ko supérieur (c'est ce que semble aussi faire l'OS Windows (?))
+            # getsize() method must take into parameters the full path of a file, i.e. his root...
+            # ... + his name. Join() method concatenate the two parameters
+            # getsize() returns a result in octets, the result was divided by 1024 for convert in Ko, then we add...
+            # ...  1 Ko for round up the result (Windows might make the same procedure for its explorer (?))
             size = (os.path.getsize(os.path.join(root, filename)) // 1024) + 1
-            # la methode getmtime() renvoie la date de la dernière modification d'un fichier...
-            # ... sous la forme d'un timestamp
+            # getmtime() method returns the date of the file's last modification...
+            # in the form of a timestamp
             timestamp = os.path.getmtime(os.path.join(root, filename))
-            # conversion timestamp en une date sous la forme DD/MM/YY HH:MM:SS
+            # timestamp conversion in the form of : DD/MM/YY HH:MM:SS
             timeFormatTemp = time.gmtime(timestamp)
             timeFormat = time.strftime("%x %X", timeFormatTemp)
             file.write("\n" + "--- " + filename + " *** " + timeFormat + " *** " + str(size) + " Ko")
             nbFiles += 1
-    file.write("\n\nNombre de fichiers dans le répertoire " + path + " : " + str(nbFiles))
+    file.write("\n\nnumber of files in the directory " + path + " : " + str(nbFiles))
 
 
 # SCRIPT ---------------------------------------------------------------------------------------------------------------
 
-# Si le répertoire de travail (celui dans lequel on souhaite stocker nos fichiers file) n'existe pas...
-# ... création de ce répertoire
+# If the work directory (the one which you should stock the file files) doesn't existe yet...
+# ... creation of this directory
 if not os.path.isdir(currentDirectory):
     os.makedirs(currentDirectory)
 
-# Désignation du répertoire de travail comme répertoire courant
+# Designation of the working directory as current directory
 os.chdir(currentDirectory)
 
-# Création du fichier file + permission pour le script d'écrire dans file
-# Il est nécessaire d'ajouter un paramètre d'encodage en UTF-8 si a un endroit de l'arborescence de fichiers...
-# ... se trouve un nom de fichier en caractères japonais ou tout autre jeu de caractères "exotiques"
+# Creating the file file + permission for the script to write in file
+# It is necessary to add a parameter of encoding in UTF-8 if there is a place of the file tree...
+# ... has a Japanese filename or any other "exotic" character set
 file = open(formatFileName(), "w", encoding="utf-8")
 
-# Ecriture des données nécessaires sur le fichier file
-print("Exécution du script slimer_script: création du (ou des) fichier(s) de sauvegarde. "
-      "Cette opération peut prendre jusqu'à plusieurs minutes...")
+print("SLIMER SCRIPT is currently running : creation of the backup file(s). "
+      "This can take up to several minutes...")
 
+# Write data in file file
 for path in paths.values():
-    file.write("\nListe des répertoires, sous-répetoires et descendants du dossier " + path + "\n")
+    file.write("\nList of directories, subdirectories and descendants of the folder " + path + "\n")
     parseDirs(path)
     file.write("\n\n################################################################################ \n")
-    file.write("\nListe des répertoires, sous-répetoires et descendants du dossier " + path + "\n")
+    file.write("\nList of files of the folder " + path + "\n")
     parseAllFoldersAndFiles(path)
     file.write("\n\n################################################################################ \n")
-    print("Script exécuté avec succès.")
+    print("It's done !.")
 
-# Fermeture du fichier file
+# File file closure
 file.close()
 
-# Copie du fichier file dans un autre répertoire de sauvegarde (un autre disque dur par exemple)
+# Copying the file file to another backup directory (another hard drive for exemple)
 if '' not in otherDirectories:
     for directory in otherDirectories.values():
         if not os.path.isdir(directory):
